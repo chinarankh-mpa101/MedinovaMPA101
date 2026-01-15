4bd90e5638787bd4c9cfcdfc16544dd2b8fed482
“Chinara <311.kamran@gmail.com>
2026-01-15 19:03:03 +0400

    ccreate added

--- MedinovaMPA101/Areas/Admin/Controllers/TeamController.cs
+++ MedinovaMPA101/Areas/Admin/Controllers/TeamController.cs
@@ -1,4 +1,5 @@
 ﻿using MedinovaMPA101.Contexts;
+using MedinovaMPA101.Models;
 using MedinovaMPA101.ViewModels.TeamViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -48,13 +49,41 @@ namespace MedinovaMPA101.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(TeamCreateVM vm)
+        public async Task<IActionResult> Create(TeamCreateVM vm)
         {
             if (!ModelState.IsValid)
             {
                 return View(vm);
             }
+            if (vm.Image.Length > 2 * 1024 * 1024)
+            {
+                ModelState.AddModelError("Image", "Image must be 2mb");
+                return View(vm);
+            }
+            if (!vm.Image.ContentType.ToLower().Contains("image"))
+            {
+                ModelState.AddModelError("Image", "Image must be image format");
+                return View(vm);
+            }
 
+            string uniqueFileName = Guid.NewGuid().ToString() + vm.Image.FileName;
+            string path = Path.Combine(_folderPath, uniqueFileName);
+            using FileStream stream = new(path, FileMode.Create);
+            await vm.Image.CopyToAsync(stream);
+
+            Team teams = new()
+
+            {
+                Name = vm.Name,
+                Position = vm.Position,
+                Description = vm.Description,
+                ImagePath=uniqueFileName
+
+            };
+
+            await _context.Teams.AddAsync(teams);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
 
         }
 
